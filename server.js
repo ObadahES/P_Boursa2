7671c90ff96cda83dc23bcc7db5dc7bbe5970a94
ObadahES <obadah.ebrahem.saadah4@gmail.com>
2025-09-20 18:58:34 +0300

    1

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(" Server Is Working On " + port + " ... "));
+// app.listen(port, () => console.log(" Server Is Working On " + port + " ... "));
+
 /********************************************************************** */
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", require("./Routes/Routes"));
+module.exports = app;
